f9f3b447ffb1c18925674d32cc2b12f01f9d6e5c
Yuanze Zhang <jason950314@gmail.com>
2018-01-12 17:59:58 -0500

    add invincible flash animation

--- Assets/Scripts/ArrowKeyMovement.cs
+++ Assets/Scripts/ArrowKeyMovement.cs
@@ -46,10 +46,10 @@ public class ArrowKeyMovement : MonoBehaviour
 		Vector2 prevPos = transform.position;
 		if (Mathf.Abs(current_input.x) > 0f){
 			float newY = Mathf.Round (prevPos.y * 2) / 2f + link_y_offset;
-			transform.position = new Vector2 (prevPos.x, newY);
+			transform.position = new Vector2 (prevPos.x, prevPos.y + (newY - prevPos.y)/2);
 		} else if (Mathf.Abs(current_input.y) > 0f){
 			float newX = Mathf.Round (prevPos.x * 2) / 2f;
-			transform.position = new Vector2 (newX, prevPos.y);
+			transform.position = new Vector2 (prevPos.x + (newX - prevPos.x)/2, prevPos.y);
 		}
 	}
 }
--- Assets/Scripts/InputToAnimator.cs
+++ Assets/Scripts/InputToAnimator.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class InputToAnimator : MonoBehaviour {
 
     Animator animator;
-
+	Player playerScript;
+	Animation animation;
+	AnimatorClipInfo[] clips;
 	// Use this for initialization
 	void Start () {
         animator = GetComponent<Animator>();
+		playerScript = GetComponent<Player> ();
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
         animator.SetFloat("horizontal_input", Input.GetAxisRaw("Horizontal"));
         animator.SetFloat("vertical_input", Input.GetAxisRaw("Vertical"));
+		animator.SetBool ("invincible", playerScript.invincible);
+		clips = animator.GetCurrentAnimatorClipInfo (0);
 
-        if(Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
+		if(Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0 && !playerScript.invincible && clips.Length > 0 && clips[0].clip.name.StartsWith("run"))
         {
             animator.speed = 0.0f;
         }
