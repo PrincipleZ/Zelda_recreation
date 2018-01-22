e7e4038e03535a6e41645fbcc5505030ee8190f1
Shawki Atassi <saatassi@umich.edu>
2018-01-22 01:02:15 -0500

    Aquamentus attack, small bugs

--- Assets/AquamentusScript.cs
+++ Assets/AquamentusScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class AquamentusScript : MonoBehaviour {
 
+    public GameObject player;
+
     public float moveSpeed = 1f;
     public GameObject fireball;
     public float attackTimeMin = 1.5f;
@@ -12,6 +14,10 @@ public class AquamentusScript : MonoBehaviour {
     float rangeMin;
     float rangeMax;
 
+    Vector3 playerPosition;
+    public Vector3 angle;
+    public float multiplyer;
+    public float yValue;
     float timeUntilAttack;
     bool startAttacking = true;
     bool isAttacking = false;
@@ -27,6 +33,17 @@ public class AquamentusScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        playerPosition = player.transform.position;
+        angle = Vector3.Normalize(transform.position - playerPosition);
+        multiplyer = Mathf.Round(angle.y * 4);
+        if(angle.x < 0)
+        {
+            yValue = 1;
+        }
+        else
+        {
+            yValue = -1;
+        }
         if (!destinationChosen)
         {
             ChooseDestination();
@@ -76,9 +93,9 @@ public class AquamentusScript : MonoBehaviour {
         GameObject fire1 = Instantiate(fireball, transform.position + new Vector3(-.1f, .5f, 0), Quaternion.identity);
         GameObject fire2 = Instantiate(fireball, transform.position + new Vector3(-.1f, .5f, 0), Quaternion.identity);
         GameObject fire3 = Instantiate(fireball, transform.position + new Vector3(-.1f, .5f, 0), Quaternion.identity);
-        fire1.transform.Rotate(0, 0, 80f);
-        fire2.transform.Rotate(0, 0, 90f);
-        fire3.transform.Rotate(0, 0, 100f);
+        fire1.transform.Rotate(0, 0, 105f + (10 * (multiplyer * -yValue)));
+        fire2.transform.Rotate(0, 0, 90f + (10 * (multiplyer * -yValue)));
+        fire3.transform.Rotate(0, 0, 75f + (10 * (multiplyer * -yValue)));
 
         fire1.transform.position += Vector3.forward;
         fire2.transform.position += Vector3.forward;
